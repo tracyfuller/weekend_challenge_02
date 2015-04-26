6913a87389132661c538f168312db99202e84fee
tracyfuller <tracy.e.fuller@gmail.com>
2015-04-25 22:28:27 -0500

    Build button class, add underscore.js library

---
+++ app.js
@@ -0,0 +1,16 @@
+var people = ["Erik", "Aaron", "Alicia", "Casie", "Clare", "Cody", "Jeanne", "Kaitlin", "Kelly", "Luke", "Mary", "Michael", "Michelle", "Rom", "Steve", "Terry", "Tracy", "Vince", "Brian", "Chelsea"];﻿
+
+
+$(document).ready(function(){
+	$()
+
+	$(".generate").click(function(){
+		if (/* a number is chosen */) {
+			// go through the function
+			// then set (something) to a boolean. If clicked again, clear everything and start over.
+		} else {
+			alert("Please pick the number of groups you wish to generate");
+		};
+	});
+
+});
