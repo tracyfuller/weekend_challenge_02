aad0dd7018991a9b4807963e5ff7b8898202a916
tracyfuller <tracy.e.fuller@gmail.com>
2015-04-26 18:27:03 -0500

    Added looping functionality. Having error.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var people = ["Erik", "Aaron", "Alicia", "Casie", "Clare", "Cody", "Jeanne", "Kaitlin", "Kelly", "Luke", "Mary", "Michael", "Michelle", "Rom", "Steve", "Terry", "Tracy", "Vince", "Brian", "Chelsea"];﻿
+var count;
 var newArray;
-var dividedTeams;
+var dividedTeams = 0;
+function clearTeams (){
+	$(".table").empty();
+}
 
 
 $(document).ready(function(){
@@ -10,17 +14,31 @@ $(document).ready(function(){
 	})
 
 	$(".generate").on("click", function(){
-		if (dividedTeams > 0) {
+		clearTeams();
+
+		if (dividedTeams == 0) {
+			alert("Please pick the number of groups you wish to generate");
+		} else {
 			newArray = _.shuffle(people);
-			// console.log("original", people);
-			// console.log("new array", newArray);
+			console.log("original", people);
+			console.log("new array", newArray);
 			for (var i = 0; i < dividedTeams; i++){
-				$(".table").append("<div class='column list" + (i+1) + "'><p><u>Team " + (i+1) + "</u></p><p>Some stuff here</p></div>");
-			}
+
+				$(".table").append("<div class='column list" + i + "'><p><u>Team " + (i+1) + "</u></p></div>");
+			};
+			count = 0 
+			for (var j = 0; j < newArray.length; j++){
+
+				if (count >= dividedTeams){
+					count = 0;
+				} else {
+					$(".list" + count).append("<p>" + newArray[j] + "</p>");
+					count++;
+				};
+			};
 			
 
-		} else {
-			alert("Please pick the number of groups you wish to generate");
+		
 		};
 	});
 
