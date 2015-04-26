e932e8518de401d5184393662822aa67541360c2
tracyfuller <tracy.e.fuller@gmail.com>
2015-04-26 18:56:18 -0500

    Fixed looping bug, added animation and color highlighting

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ function clearTeams (){
 	$(".table").empty();
 }
 
-
 $(document).ready(function(){
 	$(".numButton").click(function(){
+		$(this).siblings().removeClass("numButton-highlight");
 		dividedTeams = $(this).data("numteams");
-		console.log("number of teams chosen: " + dividedTeams);
+		$(this).addClass("numButton-highlight");
 	})
 
 	$(".generate").on("click", function(){
@@ -20,26 +20,18 @@ $(document).ready(function(){
 			alert("Please pick the number of groups you wish to generate");
 		} else {
 			newArray = _.shuffle(people);
-			console.log("original", people);
-			console.log("new array", newArray);
 			for (var i = 0; i < dividedTeams; i++){
-
 				$(".table").append("<div class='column list" + i + "'><p><u>Team " + (i+1) + "</u></p></div>");
 			};
 			count = 0 
 			for (var j = 0; j < newArray.length; j++){
 
+				$(".list" + count).append("<p>" + newArray[j] + "</p>").hide().fadeIn("fast");
+				count++;
 				if (count >= dividedTeams){
 					count = 0;
-				} else {
-					$(".list" + count).append("<p>" + newArray[j] + "</p>");
-					count++;
 				};
 			};
-			
-
-		
 		};
 	});
-
 });
