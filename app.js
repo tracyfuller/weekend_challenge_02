163bdeb6531f55be0a949689c967fa862146c67c
tracyfuller <tracy.e.fuller@gmail.com>
2015-04-26 15:35:27 -0500

    Button data set, Display set, shuffle set

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 var people = ["Erik", "Aaron", "Alicia", "Casie", "Clare", "Cody", "Jeanne", "Kaitlin", "Kelly", "Luke", "Mary", "Michael", "Michelle", "Rom", "Steve", "Terry", "Tracy", "Vince", "Brian", "Chelsea"];﻿
+var newArray;
+var dividedTeams;
 
 
 $(document).ready(function(){
-	$()
+	$(".numButton").click(function(){
+		dividedTeams = $(this).data("numteams");
+		console.log("number of teams chosen: " + dividedTeams);
+	})
 
-	$(".generate").click(function(){
-		if (/* a number is chosen */) {
-			// go through the function
-			// then set (something) to a boolean. If clicked again, clear everything and start over.
+	$(".generate").on("click", function(){
+		if (dividedTeams > 0) {
+			newArray = _.shuffle(people);
+			// console.log("original", people);
+			// console.log("new array", newArray);
+			for (var i = 0; i < dividedTeams; i++){
+				$(".table").append("<div class='column list" + (i+1) + "'><p><u>Team " + (i+1) + "</u></p><p>Some stuff here</p></div>");
+			}
+			
+
 		} else {
 			alert("Please pick the number of groups you wish to generate");
 		};
